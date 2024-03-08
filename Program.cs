027d3d40186f86c2470f5b404a24dcf7452f22e5
alanlenisa <41261509+alanlenisa@users.noreply.github.com>
2024-03-08 14:29:42 +0000

    h 1530

--- Program.cs
+++ Program.cs
@@ -8,8 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddAzureAppConfiguration(options =>
     options.Connect(
         new Uri("https://ac240307.azconfig.io"),
-        new ManagedIdentityCredential("4d8f0534-d427-46e7-b375-e268f9424bf9")));
+        new ManagedIdentityCredential()));
 
+ //new ManagedIdentityCredential("4d8f0534-d427-46e7-b375-e268f9424bf9")));
 
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestApp:Settings"));
 
