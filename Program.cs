a34583ae48e163c8338f611fe9379e10e55f324b
alanlenisa <41261509+alanlenisa@users.noreply.github.com>
2024-03-08 11:17:13 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ builder.Configuration.AddAzureAppConfiguration(options =>
         new Uri("https://ac240307.azconfig.io"),
         new ManagedIdentityCredential("4d8f0534-d427-46e7-b375-e268f9424bf9")));
 
+builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestApp:Settings"));
+
 // Add services to the container. 
 builder.Services.AddRazorPages();
 
