15f09cca2f717c9af849af76ce3eeee6cd15fe20
alanlenisa <41261509+alanlenisa@users.noreply.github.com>
2024-03-08 10:31:47 +0100

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,12 +8,13 @@ public class IndexModel : PageModel
 {
 
     public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
-    
+    public string OPT {get;set;}
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        OPT="aszx";
     }
 
     public void OnGet()
